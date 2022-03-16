2b1025f80805ca06beabea129eb4d169262fca6f
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-16 01:06:03 -0500

    User model schema and middlewares

---
+++ auth/src/models/User.ts
@@ -0,0 +1,51 @@
+import mongoose, { Schema, model } from 'mongoose';
+import { Password } from '../services/password';
+
+// An interface that describes the properties
+// that are required to create a new User
+interface UserAttrs {
+  email: string;
+  password: string;
+}
+
+// An interface that describes properties
+// that user model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
+
+// An interface that describes the properties
+// that a User Document has
+interface UserDoc extends mongoose.Document {
+  email: string;
+  password: string;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre('save', async function (done) {
+  if (this.isModified('password')) {
+    const hashed = await Password.toHash(this.get('password'));
+    this.set('password', hashed);
+  }
+  done();
+});
+
+userSchema.statics.build = (attrs: UserAttrs) => {
+  return new User(attrs);
+};
+
+const User = model<UserDoc, UserModel>('User', userSchema);
+
+export { User };
