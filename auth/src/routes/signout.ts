7e512193bab68dbf585ecc3c7e37ec3217c32a26
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-17 18:10:01 -0500

    change http method get to post

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/api/users/signout', (req, res) => {
+router.post('/api/users/signout', (req, res) => {
   req.session = null;
 
   res.send({});
