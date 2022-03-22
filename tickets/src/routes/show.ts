9979e1b35e323a61dca41cd6e986fdf8f01c4576
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-22 01:35:30 -0500

    tickets microservices implementation handler request routes api

---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,17 @@
+import { NotFoundError } from '@eahbtickets/common';
+import express, { Request, Response } from 'express';
+import { Ticket } from '../models/Ticket';
+
+const router = express.Router();
+
+router.get('/api/tickets/:id', async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  res.send(ticket);
+});
+
+export { router as showTicketRouter };
