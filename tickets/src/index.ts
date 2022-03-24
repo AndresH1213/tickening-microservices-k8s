aa9463097d82c028e93b44569b83d8e796d442f5
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-23 23:23:16 -0500

    singleton class nats wrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { app } from './app';
+import { natsWrapper } from './nats-wrapper';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -10,6 +11,7 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect('ticketing', 'sfajlk', 'http://nats-srv:4222');
     await mongoose.connect(process.env.MONGO_URI);
     console.log('Connected to MongoDB');
   } catch (error) {
