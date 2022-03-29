0a9e25ab0baa6489c3882386a68f92195eec2ae1
Andres Hernandez <edanhebla1213@hotmail.com>
2022-03-28 23:31:48 -0500

    Ticket model tests concurrency and version control

---
+++ tickets/src/models/__test__/Ticket.test.ts
@@ -0,0 +1,50 @@
+import { Ticket } from '../Ticket';
+
+it('Implements optimistic concurrency control', async () => {
+  // Create an instance of a ticket
+  const ticket = Ticket.build({
+    title: 'concert',
+    price: 5,
+    userId: '123',
+  });
+  // Save the ticket to the database
+  await ticket.save();
+  // Fetch the ticket twice
+  const firstInstance = await Ticket.findById(ticket.id);
+  const secondInstance = await Ticket.findById(ticket.id);
+
+  // Make two separate changes to the tickets we fetched
+  firstInstance!.set({ price: 10 });
+  secondInstance!.set({ price: 15 });
+  // Save the first fetched ticket
+  await firstInstance?.save();
+
+  // Save the second fetched ticket and expect an error
+  /*
+  expect(async () => {
+      await secondInstance?.save()
+  }).toThrow(); 
+  */
+  try {
+    await secondInstance?.save();
+  } catch (err) {
+    return;
+  }
+
+  throw new Error('Should not reach this point');
+});
+
+it('Increments the version number on multiple saves', async () => {
+  const ticket = Ticket.build({
+    title: 'concer',
+    price: 20,
+    userId: '123',
+  });
+
+  await ticket.save();
+  expect(ticket.version).toEqual(0);
+  await ticket.save();
+  expect(ticket.version).toEqual(1);
+  await ticket.save();
+  expect(ticket.version).toEqual(2);
+});
