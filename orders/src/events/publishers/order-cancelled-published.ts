87c043473249449dbf9c1aafc3229c393baf0399
Andres Hernandez <edanhebla1213@hotmail.com>
2022-04-03 01:08:15 -0500

    update common package

--- orders/src/events/publishers/order-cancelled-published.ts
+++
@@ -1,5 +0,0 @@
-import { Subjects, Publisher, OrderCancelledEvent } from '@eahbtickets/common';
-
-export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
-  subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
-}
--- orders/src/events/publishers/order-created-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, OrderCreatedEvent, Subjects } from '@eahbtickets/common';
-
-export class OrderCreatedPublisher extends Publisher<OrderCreatedEvent> {
-  subject: Subjects.OrderCreated = Subjects.OrderCreated;
-}
