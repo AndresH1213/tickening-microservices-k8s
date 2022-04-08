b35c86e21366235b1ad31b2b80166916481d74a8
Andres Hernandez <edanhebla1213@hotmail.com>
2022-04-07 22:17:23 -0500

    centralizing client in app component initial props

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,18 +1,41 @@
-import buildClient from '../api/build-client';
+import Link from 'next/link';
 
-const landingPage = ({ currentUser }) => {
-  return currentUser ? (
-    <h1>You are signed in</h1>
-  ) : (
-    <h1>You are not signed in</h1>
+const landingPage = ({ currentUser, tickets }) => {
+  const ticketsList = tickets.map((ticket) => {
+    return (
+      <tr key={ticket.id}>
+        <td>{ticket.title}</td>
+        <td>{ticket.price}</td>
+        <td>
+          <Link href="/tickets/[ticketId]" as={`/tickets/${ticket.id}`}>
+            <a>View</a>
+          </Link>
+        </td>
+      </tr>
+    );
+  });
+  return (
+    <div>
+      <h1>Tickets</h1>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Price</th>
+            <th>Link</th>
+          </tr>
+        </thead>
+        <tbody>{ticketsList}</tbody>
+      </table>
+    </div>
   );
 };
 
-landingPage.getInitialProps = async (context) => {
+landingPage.getInitialProps = async (context, client, currentUser) => {
   // this gets execute before rendering
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
-  return data;
+  const { data } = await client.get('/api/tickets');
+
+  return { tickets: data };
 };
 
 export default landingPage;
