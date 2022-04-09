6aaaccbff7b036d1487819076cf5df1db9890575
Andres Hernandez <edanhebla1213@hotmail.com>
2022-04-09 18:46:14 -0500

    payment microservice new route + test

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,99 @@
+import request from 'supertest';
+import { app } from '../../app';
+import mongoose from 'mongoose';
+import { Order } from '../../models/Order';
+import { OrderStatus } from '@eahbtickets/common';
+import { stripe } from '../../stripe';
+import { Payment } from '../../models/Payment';
+
+// jest.mock('../../stripe.ts');
+
+it('returns a 404 when purchasing an order that does not exist', async () => {
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'nowork',
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+
+it('returns a 401 when purchasing an order that does not belong to the user', async () => {
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId: new mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin())
+    .send({
+      token: 'nowork',
+      orderId: order.id,
+    })
+    .expect(401);
+});
+it('returns a 400 when purchasing a cancelled order', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price: 20,
+    status: OrderStatus.Cancelled,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      orderId: order.id,
+      token: 'whatever',
+    })
+    .expect(400);
+});
+
+it('returns a 201 with valid inputs', async () => {
+  const userId = new mongoose.Types.ObjectId().toHexString();
+  const price = Math.floor(Math.random() * 100000);
+  const order = Order.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    userId,
+    version: 0,
+    price,
+    status: OrderStatus.Created,
+  });
+
+  await order.save();
+
+  await request(app)
+    .post('/api/payments')
+    .set('Cookie', global.signin(userId))
+    .send({
+      token: 'tok_visa',
+      orderId: order.id,
+    })
+    .expect(201);
+
+  const stripeCharges = await stripe.charges.list({ limit: 10 });
+  const stripeCharge = stripeCharges.data.find(
+    (charge) => charge.amount === price * 100
+  );
+
+  expect(stripeCharge).toBeDefined();
+  expect(stripeCharge!.currency).toEqual('usd');
+
+  const payment = await Payment.findOne({
+    orderId: order.id,
+    stripeId: stripeCharge!.id,
+  });
+
+  expect(payment).not.toBeNull();
+});
---
+++ payments/src/routes/new.ts
@@ -0,0 +1,59 @@
+import express, { Request, Response } from 'express';
+import { body } from 'express-validator';
+import {
+  requireAuth,
+  validateRequest,
+  BadRequestError,
+  NotFoundError,
+  NotAuthorizedError,
+  OrderStatus,
+} from '@eahbtickets/common';
+import { stripe } from '../stripe';
+import { Order } from '../models/Order';
+import { Payment } from '../models/Payment';
+import { PaymentCreatedPublisher } from '../events/publishers/payment-created-publisher';
+import { natsWrapper } from '../nats-wrapper';
+
+const router = express.Router();
+
+router.post(
+  '/api/payments',
+  requireAuth,
+  [body('token').not().isEmpty(), body('orderId').not().isEmpty()],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    const { token, orderId } = req.body;
+
+    const order = await Order.findById(orderId);
+    if (!order) {
+      throw new NotFoundError();
+    }
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorizedError();
+    }
+    if (order.status === OrderStatus.Cancelled) {
+      throw new BadRequestError('This order have been cancelled');
+    }
+
+    const charge = await stripe.charges.create({
+      currency: 'usd',
+      amount: order.price * 100,
+      source: token,
+    });
+    const payment = Payment.build({
+      orderId,
+      stripeId: charge.id,
+    });
+    await payment.save();
+
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send({ id: payment.id });
+  }
+);
+
+export { router as createChargeRouter };
